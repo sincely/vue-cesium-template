41123c3005ada77bbc0bc732c61713df05498755
chengzhou <1738248438@qq.com>
2021-07-13 16:33:48 +0800

    🎨 移除无用的包；更新文件目录结构

---
+++ src/views/Viewer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="map-box">
+    <div id="cesiumContainer"></div>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import Cesium from '@/components/CesiumExample/Cesium.vue'
+
+export default {
+  name: 'Cesium',
+  components: {
+    Cesium,
+  },
+  mounted() {
+    // var viewer = new Cesium.CesiumWidget('cesiumContainer')
+
+    // eslint-disable-next-line no-undef
+    var viewer = new Cesium.Viewer('cesiumContainer')
+
+    // eslint-disable-next-line no-console
+    console.log(viewer)
+  },
+}
+</script>
+
+<style scoped>
+.map-box {
+  width: 100%;
+  height: 100%;
+}
+#cesiumContainer {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,49 @@
+'use strict'
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+const name = 'vue cesium'
+const port = process.env.port || process.env.npm_config_port || 9527 // dev port
+module.exports = {
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: process.env.NODE_ENV === 'development',
+  productionSourceMap: false,
+  devServer: {
+    port: port,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true,
+    },
+  },
+  configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    name: name,
+    resolve: {
+      alias: {
+        '@': resolve('src'),
+      },
+    },
+  },
+  chainWebpack(config) {
+    // set svg-sprite-loader
+    config.module.rule('svg').exclude.add(resolve('src/icons')).end()
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]',
+      })
+      .end()
+  },
+}

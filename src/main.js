25824ec18b2fa682768371421f79101ffa292c68
chengzhou <1738248438@qq.com>
2021-07-13 16:40:56 +0800

    🎨 去除.env环境变量

--- src/main.js
+++ src/main.js
@@ -2,12 +2,37 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axiosOrigin from 'axios'
 import 'cesium/Widgets/widgets.css'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app')
+function getServerConfig() {
+  return new Promise((resolve, reject) => {
+    axiosOrigin
+      .get('./serverConfig.json')
+      .then((result) => {
+        let config = result.data
+        for (let key in config) {
+          Vue.prototype[`$${key}`] = config[key]
+        }
+        /*  if (config.baseUrl) {
+          axios.defaults.baseURL = config.baseUrl
+        } */
+        resolve()
+      })
+      .catch(() => {
+        reject()
+      })
+  })
+}
+async function main() {
+  await getServerConfig()
+  new Vue({
+    router,
+    store,
+    render: (h) => h(App),
+  }).$mount('#app')
+}
+
+main()

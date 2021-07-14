e705803d0a53d79d3e500d3291ce67bc0c4fe4a4
chengzhou <1738248438@qq.com>
2021-07-14 08:52:36 +0800

    ✨ Viewer组件更新，增加mapUrl,baseUrl,websocketUrl

--- src/views/Viewer.vue
+++ src/views/Viewer.vue
@@ -7,10 +7,28 @@
 <script>
 export default {
   name: '',
+  data() {
+    return {
+      mapUrl: `${this.$mapUrl}/map/`, // 地图资源
+      baseUrl: this.$baseUrl,
+      viewer: null,
+      websocketUrl: this.$websocketUrl, // websocketUrl
+    }
+  },
   mounted() {
     // var viewer = new Cesium.CesiumWidget('cesiumContainer')
-    var viewer = new Cesium.Viewer('cesiumContainer')
-    // eslint-disable-next-line no-console
+    var viewer = new Cesium.Viewer('cesiumContainer', {
+      geocoder: false, //一种地理位置搜索工具，用于显示相机访问的地理位置。默认使用微软的Bing地图。
+      homeButton: true, //首页位置，点击之后将视图跳转到默认视角。
+      sceneModePicker: true, //切换2D、3D 和 Columbus View (CV) 模式。
+      baseLayerPicker: false, //选择三维数字地球的底图（imagery and terrain）。
+      navigationHelpButton: true, //帮助提示，如何操作数字地球。
+      animation: false, //控制视窗动画的播放速度。
+      creditsDisplay: false, //展示商标版权和数据源。
+      timeline: false, //展示当前时间和允许用户在进度条上拖动到任何一个指定的时间。
+      fullscreenButton: true, //视察全屏按钮
+    })
+    viewer.scene.globe.enableLighting = true //启用以太阳为光源的地球
     console.log(viewer)
   },
 }
